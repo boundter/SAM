bfa94228605bf32e5e7a7ae95ce2380d98eec60b
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-12-19 17:26:06 +0100

    Refactor: Implementation closer to declaration

--- include/sam/system/rk4_system.hpp
+++ include/sam/system/rk4_system.hpp
@@ -30,8 +30,24 @@ class RK4System: public GenericSystem<ODE, state_type> {
   boost::numeric::odeint::runge_kutta4<state_type> stepper_;
 };
 
-}  // namespace sam
+// Implementation
 
-#include <sam/system/detail/rk4_system.hpp>
+template<typename ODE, typename state_type>
+template<typename... Ts>
+RK4System<ODE, state_type>::RK4System(unsigned int system_size,
+                                      unsigned int dimension,
+                                      Ts... parameters)
+    : GenericSystem<ODE, state_type>(system_size, dimension, parameters...) {}
+
+template<typename ODE, typename state_type>
+template<typename observer_type>
+void RK4System<ODE, state_type>::Integrate(double dt, unsigned int number_steps,
+                                           observer_type observer) {
+      this->t_ = boost::numeric::odeint::integrate_n_steps(
+          stepper_, *(this->ode_), this->x_, this->t_, dt, number_steps,
+          observer);
+}
+
+}  // namespace sam
 
 #endif  // INCLUDE_SAM_SYSTEM_RK4_SYSTEM_HPP_
