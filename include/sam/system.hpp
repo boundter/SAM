f3f291db3eab6fd269d2a36f95134cf29823d868
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-12-20 14:53:57 +0100

    Add: Single header for system include

---
+++ include/sam/system.hpp
@@ -0,0 +1,10 @@
+// Copyright 2019 Erik Teichmann <kontakt.teichmann@gmail.com>
+
+#ifndef INCLUDE_SAM_SYSTEM_HPP_
+#define INCLUDE_SAM_SYSTEM_HPP_
+
+#include <sam/system/generic_system.hpp>
+#include <sam/system/euler_system.hpp>
+#include <sam/system/rk4_system.hpp>
+
+#endif  // INCLUDE_SAM_SYSTEM_HPP_
