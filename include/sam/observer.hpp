6847dd880902bd64553539bc1930a812ce32f6e6
Erik Teichmann <kontakt.teichmann@gmail.com>
2019-12-20 15:10:16 +0100

    Add: Single include file for observers

---
+++ include/sam/observer.hpp
@@ -0,0 +1,8 @@
+// Copyright 2019 Erik Teichmann <kontakt.teichmann@gmail.com>
+
+#ifndef INCLUDE_SAM_OBSERVER_HPP_
+#define INCLUDE_SAM_OBSERVER_HPP_
+
+#include <sam/observer/position_observer.hpp>
+
+#endif  // INCLUDE_SAM_OBSERVER_HPP_
